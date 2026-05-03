cc5f48050481592ac32c44dff3227ff719b45324
tsuna-soboro <yuuuka110@gmail.com>
2026-05-03 14:23:18 +0900

    タイトル画面(仮)

---
+++ Jutch/Assets/ButtonManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    public int selectedstage=1;
+    public GameObject SelectPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SelectPanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnButtonClick()
+    {
+        SelectPanel.SetActive(true);
+    }
+
+    public void OnClickStage1()
+    {
+        selectedstage = 1;
+    }
+    public void OnClickStage2()
+    {
+        selectedstage = 2;
+    }
+    public void OnClickStage3()
+    {
+        selectedstage = 3;
+    }
+    public void OnClickStage4()
+    {
+        selectedstage = 4;
+    }
+    public void OnClickStage5()
+    {
+        selectedstage = 5;
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Stage" + selectedstage);
+    }   
+}
