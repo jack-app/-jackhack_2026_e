f53dd2921d894d1c4a32e8338b86facd7a9ddd33
faiyakitori <faiyakitori@outlook.jp>
2026-05-04 00:07:46 +0900

    ちょい修正

--- Jutch/Assets/MainCharacter.cs
+++ Jutch/Assets/MainCharacter.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -20,7 +20,7 @@ void Update()
         myTransform.position = pos; //座標を設定
 
       }
-      
+      */
 
 
     
