300c4f735eeaf1377795c0c84a2c6bce4f352d32
faiyakitori <faiyakitori@outlook.jp>
2026-05-04 12:51:10 +0900

    違法建築2

--- Jutch/Assets/on_off_block.cs
+++ Jutch/Assets/on_off_block.cs
@@ -5,29 +5,32 @@ using UnityEngine;
 public class on_off_block : MonoBehaviour
 {
     public Collider2D col ;
+    SpriteRenderer sprite;
+    Color color;
 
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         col = transform.GetComponent<Collider2D>(); 
-        Color color = gameObject.GetComponent<Image>().color;
+        sprite = gameObject.GetComponent<SpriteRenderer>();
+        color = sprite.color;
         OnOffBlock(0);  
     }
 
-       public int block_color = 0;
+    public int block_color = 0;
     public void OnOffBlock(int player_state)
     {
-        if(block_color == player_state)
+        if(block_color != player_state)
         {
-            col.enabled = true;
+            col.enabled = false;
             color.a = 0.3f;
         }
         else
         {
-            col.enabled = false;
+            col.enabled = true;
             color.a = 1.0f;
         }
-        gameObject.GetComponent<Image>().color = block_color;
+        sprite.color = color;
     }
 }
--- Jutch/Assets/ryyuzaki_test.cs
+++ Jutch/Assets/ryyuzaki_test.cs
@@ -6,18 +6,26 @@ public class ryyuzaki_test : MonoBehaviour
 {
     // 0が青、１が緑
     public int player_state = 0;
-    on_off_block[] blocks;
+    on_off_block[] oobs;
 
     // Start is called before the first frame update
     void Start()
     {
-        blocks = GameObject.FindGameObjectsWithTag("Finish").GetComponent<on_off_block>();
+        GameObject[] blocks = GameObject.FindGameObjectsWithTag("OOB");
+        Debug.Log($"blockCount:{blocks.Length}");
+        oobs = new on_off_block[blocks.Length];
+        for(int i = 0; i < blocks.Length; i++)
+        {
+            oobs[i] = blocks[i].GetComponent<on_off_block>();
+            oobs[i].OnOffBlock(player_state);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown("E")) {
+        if(Input.GetKeyDown(KeyCode.E)) {
+            Debug.Log("key:e");
             if(player_state == 0)
             {
                 player_state = 1;
@@ -27,9 +35,9 @@ public class ryyuzaki_test : MonoBehaviour
                 player_state = 0;
             }
 
-            foreach(on_off_block block in blocks)
+            foreach(on_off_block oob in oobs)
             {
-                block.OnOffBlock(player_state);
+                oob.OnOffBlock(player_state);
             }
         }      
      
