3dc039220daa5ac601c827e9e0588878bb460be9
ryuuzaki <aburithizu@gmail.com>
2026-05-04 12:10:58 +0900

    違法建築前夜

--- Jutch/Assets/ryyuzaki_test.cs
+++ Jutch/Assets/ryyuzaki_test.cs
@@ -6,11 +6,12 @@ public class ryyuzaki_test : MonoBehaviour
 {
     // 0が青、１が緑
     public int player_state = 0;
+    on_off_block[] blocks;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        blocks = GameObject.FindGameObjectsWithTag("Finish").GetComponent<on_off_block>();
     }
 
     // Update is called once per frame
@@ -24,6 +25,11 @@ public class ryyuzaki_test : MonoBehaviour
             
             else {
                 player_state = 0;
+            }
+
+            foreach(on_off_block block in blocks)
+            {
+                block.OnOffBlock(player_state);
             }
         }      
      
